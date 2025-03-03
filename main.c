3af9688e15146c59ace27713519c5e2dd5f0a5da
Cong Nguyen <72590623+EmbeddedOS@users.noreply.github.com>
2025-03-03 17:27:06 +0700

    Update main.c

--- main.c
+++ main.c
@@ -3,46 +3,16 @@ volatile unsigned int *const UART0DR = (unsigned int *)0x09000000;
 void print_uart0(const char *s)
 {
     while (*s != '\0')
-    {                                  /* Loop until end of string */
-        *UART0DR = (unsigned int)(*s); /* Transmit char */
-        s++;                           /* Next char */
+    {                             
+        *UART0DR = (unsigned int)(*s); 
+        s++;                         
     }
-}
-
-int x = 0;
-
-void __attribute__((naked)) svc_handler_c()
-{
-    
 }
 
 void main()
 {
-    long result;
-   // asm volatile("mov %0, x1" : "=r"(result)::);
-    print_uart0("hello\n");
-    if (result == 0)
-    {
-        print_uart0("0\n");
-    }
-    else if (result & 0b0100)
-    {
-        print_uart0("1\n");
-    }
-    else if (result & 0b1000)
-    {
-        print_uart0("2\n");
-    }
-    else if (result & 0b1100)
-    {
-        print_uart0("3\n");
-    }
-
-    // if (x == 1)
-    // {
-    // print_uart0("Hello word\n");
-
-    // }
-    
+    print_uart0("Hello world!\n");
+    asm("svc #0");
+    print_uart0("Hello world!\n");
 
 }
