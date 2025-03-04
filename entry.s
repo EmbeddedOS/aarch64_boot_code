6f7499ccb26339db0619d2732c19230e5fbd4dd6
Cong Nguyen <72590623+EmbeddedOS@users.noreply.github.com>
2025-03-04 18:35:28 +0700

    Update entry.s

--- entry.s
+++ entry.s
@@ -1,64 +1,136 @@
 
 .text
-.globl vector_table_el1
+.globl vector_table_el1, vector_table_el2, vector_table_el3
 
 // Vector tables must be placed at a 2KB-aligned address.
 
-.balign 2048
+.macro qemu_print reg, len
+    mov x10, #0x09000000
+    mov x11, #\len
+    mov x12, #\reg
+1:
+    ldrb w2, [x12] /* Get the first byte and clear all the rest. */
+    str w2, [x10]
+    cmp x11, #0
+    beq 2f
+    sub x11, x11, #1
+    add x12, x12, #1
+    b 1b
+2:
+.endm
+
+.balign 0x800
 vector_table_el1:
+vector_table_el2:
 // Exception from current EL while using SP_EL0.
-curr_el_sp0_sync:
-    b .
+el1_curr_el_sp0_sync:
+    qemu_print error, error_len
 .balign 0x80
-curr_el_sp0_irq:
-    b .
+el1_curr_el_sp0_irq:
+    qemu_print error, error_len
 .balign 0x80
-curr_el_sp0_fiq:
-    b .
+el1_curr_el_sp0_fiq:
+    qemu_print error, error_len
 .balign 0x80
-curr_el_sp0_serror:
-    b .
+el1_curr_el_sp0_serror:
+    qemu_print error, error_len
 .balign 0x80
 // Exception from current EL while using SP_EL1.
-curr_el_sp1_sync:
-    b .
+el1_curr_el_sp1_sync:
+    qemu_print error, error_len
 .balign 0x80
-curr_el_sp1_irq:
-    b .
+el1_curr_el_sp1_irq:
+    qemu_print error, error_len
 .balign 0x80
-curr_el_sp1_fiq:
-    b .
+el1_curr_el_sp1_fiq:
+    qemu_print error, error_len
 .balign 0x80
-curr_el_sp1_serror:
-    b .
+el1_curr_el_sp1_serror:
+    qemu_print error, error_len
 .balign 0x80
 
 // Exception from a lower EL and at least one lower EL is AArch64.
-lower_el_aarch64_sync:
+el1_lower_el_aarch64_sync:
     bl sync_handler
 .balign 0x80
-lower_el_aarch64_irq:
-    b .
+el1_lower_el_aarch64_irq:
+    qemu_print error, error_len
 .balign 0x80
-lower_el_aarch64_fiq:
-    b .
+el1_lower_el_aarch64_fiq:
+    qemu_print error, error_len
 .balign 0x80
-lower_el_aarch64_serror:
-    b .
+el1_lower_el_aarch64_serror:
+    qemu_print error, error_len
 .balign 0x80
 
 // Exception from a lower EL and all lower ELs are AArch32.
-lower_el_aarch32_sync:
-    b .
+el1_lower_el_aarch32_sync:
+    qemu_print error, error_len
 .balign 0x80
-lower_el_aarch32_irq:
-    b .
+el1_lower_el_aarch32_irq:
+    qemu_print error, error_len
 .balign 0x80
-lower_el_aarch32_fiq:
-    b .
+el1_lower_el_aarch32_fiq:
+    qemu_print error, error_len
 .balign 0x80
-lower_el_aarch32_serror:
-    b .
+el1_lower_el_aarch32_serror:
+    qemu_print error, error_len
+
+.balign 0x800
+vector_table_el3:
+// Exception from current EL while using SP_EL0.
+el3_curr_el_sp0_sync:
+    qemu_print error, error_len
+.balign 0x80
+el3_curr_el_sp0_irq:
+    qemu_print error, error_len
+.balign 0x80
+el3_curr_el_sp0_fiq:
+    qemu_print error, error_len
+.balign 0x80
+el3_curr_el_sp0_serror:
+    qemu_print error, error_len
+.balign 0x80
+// Exception from current EL while using SP_EL1.
+el3_curr_el_sp1_sync:
+    qemu_print error, error_len
+.balign 0x80
+el3_curr_el_sp1_irq:
+    qemu_print error, error_len
+.balign 0x80
+el3_curr_el_sp1_fiq:
+    qemu_print error, error_len
+.balign 0x80
+el3_curr_el_sp1_serror:
+    qemu_print error, error_len
+.balign 0x80
+
+// Exception from a lower EL and at least one lower EL is AArch64.
+el3_lower_el_aarch64_sync:
+    qemu_print error, error_len
+.balign 0x80
+el3_lower_el_aarch64_irq:
+    qemu_print error, error_len
+.balign 0x80
+el3_lower_el_aarch64_fiq:
+    qemu_print error, error_len
+.balign 0x80
+el3_lower_el_aarch64_serror:
+    qemu_print error, error_len
+.balign 0x80
+
+// Exception from a lower EL and all lower ELs are AArch32.
+el3_lower_el_aarch32_sync:
+    qemu_print error, error_len
+.balign 0x80
+el3_lower_el_aarch32_irq:
+    qemu_print error, error_len
+.balign 0x80
+el3_lower_el_aarch32_fiq:
+    qemu_print error, error_len
+.balign 0x80
+el3_lower_el_aarch32_serror:
+    qemu_print error, error_len
 
 sync_handler:
     mov x14, 0x09000000
@@ -111,3 +183,5 @@ sync_handler:
 
     eret
 
+error: .asciz "Error happen\n"
+error_len = . - error - 1
