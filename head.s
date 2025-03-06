bf1ad0d03348169fb81572dc3e83d8579ff54b94
Cong Nguyen <72590623+EmbeddedOS@users.noreply.github.com>
2025-03-06 17:41:37 +0700

    Update head.s

--- head.s
+++ head.s
@@ -1,34 +1,63 @@
 .text
-
-.globl _start, svc_handler
+.globl _start
 
 /*
  * @brief - This function write characters into UART Tx Register that based on
  *          AArch64 `virt` machine.
  */
 .macro qemu_print reg, len
-    // str x10, [sp, #64]!
-    // str x11, [sp, #128]!
-    // str x12, [sp, #192]!
+    sub sp, sp, #24
+    str x10, [sp, #0]
+    str x11, [sp, #8]
+    str x12, [sp, #16]
 
     mov x10, #0x09000000
     mov x11, #\len
-    mov x12, #\reg
+    ldr x12, =\reg
 1:
     ldrb w2, [x12] /* Get the first byte and clear all the rest. */
-    str w2, [x10]
-    cmp x11, #0
+    strb w2, [x10]
+    cmp x11, #1
     beq 2f
     sub x11, x11, #1
     add x12, x12, #1
     b 1b
 2:
-    // ldr   x12, [sp], #64
-    // ldr   x11, [sp], #128
-    // ldr   x10, [sp], #192
+    ldr x12, [sp, #16]
+    ldr x11, [sp, #8]
+    ldr x10, [sp, #0]
+    add sp, sp, #24
 .endm
 
+/* If the firmware is run as ROM code, QEMU have no idea about the entry point. 
+ * So the CPU will start execute from 0x00000000, we perform relocating our 
+ * application to RAM. */
+_relocate:
+    mov x0, 0x00
+    ldr x1, =_end_copy_image
+    mov x2, #0x40000000
+    sub x1, x1, x2
+
+    // x0 holds start, x2 holds destination and x1 holds the copy size.
+copy_loop:
+    ldr x3, [x0]
+    str x3, [x2]
+
+    add x2, x2, #4
+    add x0, x0, #4
+    sub x1, x1, #4
+    cmp x1, #0
+    bne copy_loop
+
+    /* After loading, we jump into the application entry point on RAM. */
+    ldr x1, =_start
+    br x1
+
+/* Here is actually our application entry point, if the firmware is run as
+ * kernel. QEMU will read the entry point from ELF file, and jump directly to
+ * this address, ignore _relocate code. */
 _start:
+    /* We don't know current EL yet, so we load a generic stack pointer. */
     ldr x30, =stack_top
     mov sp, x30
 
@@ -64,20 +93,7 @@ in_el3:
     mrs x0, scr_el3         /* Configure Secure Control Register:             */
     orr x0, x0, #(1<<10)    /* EL2 exception state is AArch64.                */
     orr x0, x0, #(1<<0)     /* Non Secure state for EL1.                      */
-    orr x0, x0, #(1<<1)
-    orr x0, x0, #(1<<2)
-    orr x0, x0, #(1<<3)
-    and x0, x0, #(~(1<<62))
     msr scr_el3, x0
-
-    mrs x0, sctlr_el2
-    orr x0, x0, #(1<<25)
-    orr x0, x0, #(1<<12)
-    orr x0, x0, #(1<<1)
-    orr x0, x0, #(1<<2)
-    orr x0, x0, #(1<<3)
-    msr sctlr_el2, x0
-
 
     mov x0, #0b000001001    /* DAIF[8:5]=0000 M[4:0]=01001 EL0 state:         */
     msr spsr_el3, x0        /* Select EL2 with SP_EL2.                        */
@@ -102,7 +118,7 @@ in_el2:
     orr x0, x0, #(1<<31)
     msr hcr_el2, x0
 
-    mov x0, #0b111100101    /* DAIF[8:5]=0000 M[4:0]=00101 The state indicate */
+    mov x0, #0b000000101    /* DAIF[8:5]=0000 M[4:0]=00101 The state indicate */
     msr spsr_el2, X0        /* EL1 with SP_EL1.                               */
 
     ldr x30, =el1_stack_top
@@ -117,10 +133,10 @@ in_el1:
     qemu_print in_el1_message, in_el1_message_len
 
     /* Set up Execution state before return to EL0. */
-    ldr x1, =vector_table_el1       /* Load EL1 vector table.                 */ 
+    ldr x1, =vector_table_el1
     msr vbar_el1, x1
 
-    mov x0, #0b111100000    /* DAIF[8:5]=0000 M[4:0]=00000 EL0 state.             */
+    mov x0, #0b000000000    /* DAIF[8:5]=0000 M[4:0]=00000 EL0 state.         */
     msr spsr_el1, x0
 
     ldr x30, =el0_stack_top
@@ -128,31 +144,24 @@ in_el1:
 
     adr x0, in_el0
     msr elr_el1, x0
+    eret
 
 in_el0:
     qemu_print in_el0_message, in_el0_message_len
-    svc 1
-    bl main
-
+    bl el0_main
     b .
 
-svc_handler:
-    qemu_print svc_message, svc_message_len
-
-svc_message: .asciz "SVC handler!\n"
-svc_message_len = . - svc_message - 1
-
 hello_message: .asciz "Aarch64 bare metal code!\n"
-hello_message_len = . - hello_message - 1
+hello_message_len = . - hello_message
 
 in_el0_message: .asciz "In EL0!\n"
-in_el0_message_len = . - in_el0_message - 1
+in_el0_message_len = . - in_el0_message
 
 in_el1_message: .asciz "In EL1!\n"
-in_el1_message_len = . - in_el1_message - 1
+in_el1_message_len = . - in_el1_message
 
 in_el2_message: .asciz "In EL2!\n"
-in_el2_message_len = . - in_el2_message - 1
+in_el2_message_len = . - in_el2_message
 
 in_el3_message: .asciz "In EL3!\n"
-in_el3_message_len = . - in_el3_message - 1
+in_el3_message_len = . - in_el3_message
