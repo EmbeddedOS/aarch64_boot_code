4c4d4724c5837d060de0260a7d63653c9930ca24
Cong Nguyen <72590623+EmbeddedOS@users.noreply.github.com>
2025-03-06 17:42:35 +0700

    Update entry.s

--- entry.s
+++ entry.s
@@ -2,142 +2,134 @@
 .text
 .globl vector_table_el1, vector_table_el2, vector_table_el3
 
-// Vector tables must be placed at a 2KB-aligned address.
-
 .macro qemu_print reg, len
+    sub sp, sp, #24
+    str x10, [sp, #0]
+    str x11, [sp, #8]
+    str x12, [sp, #16]
+
     mov x10, #0x09000000
     mov x11, #\len
-    mov x12, #\reg
+    ldr x12, =\reg
 1:
     ldrb w2, [x12] /* Get the first byte and clear all the rest. */
-    str w2, [x10]
-    cmp x11, #0
+    strb w2, [x10]
+    cmp x11, #1
     beq 2f
     sub x11, x11, #1
     add x12, x12, #1
     b 1b
 2:
+    ldr   x12, [sp, #16]
+    ldr   x11, [sp, #8]
+    ldr   x10, [sp, #0]
+    add sp, sp, #24
 .endm
+
+// Vector tables must be placed at a 2KB-aligned address.
 
 .balign 0x800
 vector_table_el1:
 vector_table_el2:
-// Exception from current EL while using SP_EL0.
 el1_curr_el_sp0_sync:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el1_curr_el_sp0_irq:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el1_curr_el_sp0_fiq:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el1_curr_el_sp0_serror:
-    qemu_print error, error_len
+    b .
 .balign 0x80
-// Exception from current EL while using SP_EL1.
 el1_curr_el_sp1_sync:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el1_curr_el_sp1_irq:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el1_curr_el_sp1_fiq:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el1_curr_el_sp1_serror:
-    qemu_print error, error_len
+    b .
 .balign 0x80
-
-// Exception from a lower EL and at least one lower EL is AArch64.
 el1_lower_el_aarch64_sync:
-    bl sync_handler
+    b sync_handler
 .balign 0x80
 el1_lower_el_aarch64_irq:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el1_lower_el_aarch64_fiq:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el1_lower_el_aarch64_serror:
-    qemu_print error, error_len
+    b .
 .balign 0x80
-
-// Exception from a lower EL and all lower ELs are AArch32.
 el1_lower_el_aarch32_sync:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el1_lower_el_aarch32_irq:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el1_lower_el_aarch32_fiq:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el1_lower_el_aarch32_serror:
-    qemu_print error, error_len
+    b .
 
 .balign 0x800
 vector_table_el3:
-// Exception from current EL while using SP_EL0.
 el3_curr_el_sp0_sync:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el3_curr_el_sp0_irq:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el3_curr_el_sp0_fiq:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el3_curr_el_sp0_serror:
-    qemu_print error, error_len
+    b .
 .balign 0x80
-// Exception from current EL while using SP_EL1.
 el3_curr_el_sp1_sync:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el3_curr_el_sp1_irq:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el3_curr_el_sp1_fiq:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el3_curr_el_sp1_serror:
-    qemu_print error, error_len
+    b .
 .balign 0x80
-
-// Exception from a lower EL and at least one lower EL is AArch64.
 el3_lower_el_aarch64_sync:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el3_lower_el_aarch64_irq:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el3_lower_el_aarch64_fiq:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el3_lower_el_aarch64_serror:
-    qemu_print error, error_len
+    b .
 .balign 0x80
-
-// Exception from a lower EL and all lower ELs are AArch32.
 el3_lower_el_aarch32_sync:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el3_lower_el_aarch32_irq:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el3_lower_el_aarch32_fiq:
-    qemu_print error, error_len
+    b .
 .balign 0x80
 el3_lower_el_aarch32_serror:
-    qemu_print error, error_len
+    b .
 
 sync_handler:
-    mov x14, 0x09000000
-    mov x15, 'A'
-    str x15, [x14]
-    bl svc_handler
-    eret
     stp x20, x21, [sp, #-16]!
 
     mov x21, sp
@@ -183,5 +175,9 @@ sync_handler:
 
     eret
 
-error: .asciz "Error happen\n"
+svc_handler:
+    qemu_print error, error_len
+    ret
+
+error: .asciz "SVC handler\n"
 error_len = . - error - 1
